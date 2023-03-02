c34dbe8bbcd6e67dcdab12ece9c5b8d2723f78f1
Sarthak Srivastava <sarthaksri2003@gmail.com>
2023-03-02 12:20:04 +0530

    Navbar, Movie Carousel, Popular film list and a film's page is done.

---
+++ client/src/components/Card/Card.js
@@ -0,0 +1,44 @@
+  import React, {useEffect, useState} from "react";
+  import Skeleton, {SkeletonTheme} from "react-loading-skeleton"
+  import "./Card.css"
+  import { Link } from "react-router-dom";
+
+
+  const Cards =  ({movie}) => {
+    console.log(movie);
+    const [Loading, SetLoading] = useState(true)
+
+    useEffect(() => {
+      setTimeout(() => {
+        SetLoading(false)
+      }, 1500)
+    }, [])
+
+    return <>
+    {
+      Loading 
+      ?
+      <div className="card">
+        <SkeletonTheme color = "#202020" highlightColor="#444">
+          <Skeleton height={300} duration = {20} />
+        </SkeletonTheme>
+      </div>
+      :
+      <Link to={`/movie/${movie.id}`} style = {{textDecoration: "none", color: "white"}}>
+        <div className="card">
+          <img className="cards-img" src={`https://image.tmdb.org/t/p/original${movie?movie.poster_path:""}`} />
+          <div className="card-overlay">
+            <div className="card-title">{movie?movie.original?.title:""}</div>
+            <div className="card-runtime">
+              {movie?movie.release_date:""}
+              <span className="card-rating">{movie?movie.vote_average:""}</span>
+            </div>
+            <div className="card-desc">{movie?movie.overview.slice(0,118) + "..." : "" }</div>
+          </div>
+        </div>
+      </Link>
+    }
+    </>
+  }
+
+  export default Cards;
---
+++ client/src/components/List/List.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from "react"
+import Cards from "../Card/Card" 
+import "./List.css"
+import { useParams } from "react-router-dom"
+
+const MovieList = () => {
+
+    const [mList, setmList] = useState([])
+    const {type} = useParams()
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    useEffect(() => {
+        getData()
+    }, [type])
+
+    const getData = () => {
+        fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
+        .then(res => res.json())
+        .then(data => setmList(data.results))
+    }
+console.log(mList)
+    return (
+        <div className="list">
+            <h2 className="listTitle">{(type ? type : "Popular")}</h2>
+            <div className="listCard">
+                {
+                    mList.map((movie,index) => (
+                        <Cards key={index} movie = {movie} />
+                    ))
+                }
+            </div>
+        </div>
+    )
+  
+}
+
+export default MovieList
